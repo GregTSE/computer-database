c18ba5b12cd9657007bdb8e1b2a3449d5fe8c583
GregTSE <gregori.tirsatine@gmail.com>
2015-03-06 16:32:11 +0100

    Add tests for dao

---
+++ src/test/java/com/excilys/formation/cdb/persistence/implementation/TestComputerDAO.java
@@ -0,0 +1,24 @@
+package com.excilys.formation.cdb.persistence.implementation;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestComputerDAO {
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+//    @Test
+//    public void test() {
+//	fail("Not yet implemented");
+//    }
+
+}
